f76d1342912618c2ba78852387df4580c0b31d85
klickers <bluebirid228@gmail.com>
2024-09-26 11:05:32 +0300

    Update dateFormat.ts

--- src/scripts/dateFormat.ts
+++ src/scripts/dateFormat.ts
@@ -1,7 +1,10 @@
 import { differenceInCalendarDays, format, isSameDay } from "date-fns"
 
 export default function dateFormat(date: number) {
-	if (isSameDay(new Date(), date)) return format(date, "hh:mmbbb")
+	if (isSameDay(new Date(), date))
+		return format(date, "bbb") == "am" || format(date, "bbb") == "pm"
+			? format(date, "hh:mmbbb")
+			: format(date, "bbb")
 	else if (differenceInCalendarDays(new Date(), date) < 7)
 		return format(date, "EEEE")
 	else return format(date, "MMM dd")
